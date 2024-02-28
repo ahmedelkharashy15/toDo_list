37621fcd91ab53ccbfbbe96e32f7c71a8cc54666
Ahmed Elkhrashey <ahmedelkhrashey@gmail.com>
2024-02-29 01:07:46 +0200

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './fonts/ShadowsIntoLight-Regular.ttf'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  
+  const [x , setx] = useState([])
+
+  const inputRef = useRef()
+
+  const add = () => {
+    const task = {taskDone : false , value : inputRef.current.value}
+
+    setx([...x , task])
+
+    inputRef.current.value = ""
+  }
+
+  const done = (index) => {
+    const newState = [...x]
+
+    newState[index].taskDone = !newState[index].taskDone
+
+    setx(newState)
+  }
+
+  const deleteTask = (index) => {
+    const newState = [...x]
+
+    newState.splice(index , 1)
+
+    setx(newState)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mx-auto col-11 col-sm-9 col-md-7 col-lg-5">
+      <h2>ToDo List</h2>
+
+      <ul>
+        {
+          x.map(({taskDone , value} , index) => {
+            return <div className='item'>
+                    <li onClick={() => done(index)} className={taskDone ? "taskDone" : ""}> {value} </li>
+                    <span onClick={() => deleteTask(index)}><i class="far fa-times-circle"></i></span>
+            </div>
+          })
+        }
+      </ul>
+
+      <div className='input-btn'>
+        <input ref={inputRef} placeholder='Enter new task...'></input>
+        <button onClick={add}><i class="fas fa-plus"></i></button>
+      </div>
     </div>
   );
 }
